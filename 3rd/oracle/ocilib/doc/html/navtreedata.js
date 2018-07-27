8a99d738e791abdc3eeaca05d956c381789b5ba2
runner0621 <31098990+runner0621@users.noreply.github.com>
2018-07-27 16:07:37 +0800

    Init version submit, version: 1.0

---
+++ 3rd/oracle/ocilib/doc/html/navtreedata.js
@@ -0,0 +1,54 @@
+var NAVTREE =
+[
+  [ "OCILIB (C and C++ Driver for Oracle)", "index.html", [
+    [ "Introduction", "index.html", [
+      [ "Description", "index.html#Intro", null ],
+      [ "Version information", "index.html#Version", null ],
+      [ "Main features", "index.html#Features", null ],
+      [ "Download", "index.html#Download", null ],
+      [ "Author", "index.html#Author", null ],
+      [ "License", "index.html#License", null ],
+      [ "ChangeLog", "index.html#Changelog", null ]
+    ] ],
+    [ "Documentation", "modules.html", "modules" ],
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"classocilib_1_1_direct_path.html#a71aed9b74cd735c8b4baecdc284af97baa66504118eb7e793504257fe045fdace",
+"classocilib_1_1_lob.html#ab406fa9ef9a54748fa037d91d073dea3",
+"classocilib_1_1_subscription.html#aa2667ca0384af70f0c17d6bdc28ed082",
+"group___ocilib_c_api_binding.html#ga2146a1759cab308a0b4afcb1726913ae",
+"group___ocilib_c_api_feature_returning_into.html#ga24570345dff59944656f2cf537de0693",
+"group___ocilib_c_api_raw_handles.html#gaa77980531a35fa442bfa3ef49f1a87af"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
